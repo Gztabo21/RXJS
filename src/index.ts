e2254ef1150d5afb515c2c157c6b4c1811968b4f
Gztabo21 <Gustavocacharuco@gmail.com>
2020-04-21 02:03:06 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+import {Observable}from 'rxjs';
 
 
-console.log('Hola Mundo!');
+//const obs$=Observable.create();
 
+const obs$ = new Observable( subs=>{
+ subs.next('');   
+});
 
-
-
+obs$.subscribe(console.log);2
 
 
 
