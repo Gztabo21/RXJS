054b7292dcb9a32fe202a502cb0964d129f20e8c
Gztabo21 <Gustavocacharuco@gmail.com>
2020-05-13 23:39:42 -0400

    nuevos cambios

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-import { of } from "rxjs";
-import { take } from "rxjs/operators";
 
-const number$ = of(1,2,3,4,5,6);
 
-number$.pipe(
-    take(3)
-).subscribe(
-    {
-        next: vals =>(console.log('valor:',vals)),
-        complete: ()=>(console.log)
-    }
-)
+
 
---
+++ src/operators/07-tap.ts
@@ -0,0 +1,16 @@
+import { of, fromEvent } from "rxjs";
+import { take } from "rxjs/operators";
+
+const number$ = of(1,2,3,4,5,6);
+
+number$.pipe(
+    take(3)
+).subscribe(
+    {
+        next: vals =>(console.log('valor:',vals)),
+        complete: ()=>(console.log)
+    }
+)
+
+
+
